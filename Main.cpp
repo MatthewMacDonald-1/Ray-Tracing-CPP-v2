2318af6fa2cce6e50a0d250ca6ecdc0c8a6f17f4
Raptor_854 <113599350+MatthewMacDonald-1@users.noreply.github.com>
2023-08-15 16:00:51 +1200

    changes

--- Main.cpp
+++ Main.cpp
@@ -111,12 +111,12 @@ int main() {
 	// const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 512;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 10;
+	const int samples_per_pixel = 100;
 	const int max_depth = 10;
 
 	// World
 	auto R = cos(pi/4);
-	hittable_list world = random_scene();
+	hittable_list world = scene_a();
 
 
 
@@ -133,7 +133,7 @@ int main() {
 	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; --j) {
-		std::cerr << "\rScan lines remaining: " << j << ' ' << std::flush;
+		std::cerr << "\rScan lines remaining: " << j << " / " << (image_height) << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
 			color pixel_color(0, 0, 0);
 			for (int s = 0; s < samples_per_pixel; ++s) {
